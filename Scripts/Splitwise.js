0191629e8c9a7aebe37028a6c910864fda977878
Alonso Alejandro Zúñiga Beltrán <aazbeltran@outlook.com>
2021-05-12 00:03:54 -0500

    feat: add more cases on splitewise module

--- Scripts/Splitwise.js
+++ Scripts/Splitwise.js
@@ -1,4 +1,9 @@
-const obj = JSON.parse($response.body);
+let obj = {};
+try {
+    obj = JSON.parse($response.body);
+} catch (htmlBody) {
+    $done({});
+}
 
 if (obj.user !== undefined) {
     obj.user = {...obj.user, pro: true};
@@ -6,11 +11,21 @@ if (obj.user !== undefined) {
 
 if (obj.metadata !== undefined && obj.metadata.features !== undefined) {
     Object.entries(obj.metadata.features).map(([key, props]) => {
+        if (key === 'pro_settings') {
+            props.visible = false;
+        }
         if (props.enabled !== undefined) {
             props.enabled = true;
         }
         obj.metadata.features[key] = props;
     })
+}
+
+if (obj.subscriptions !== undefined) {
+    obj.subscriptions = obj.subscriptions.map(subscription => ({
+        ...subscription,
+        expires_at: '2099-12-31T23:59:59-05:00'
+    }))
 }
 
 $done({body: JSON.stringify(obj)});

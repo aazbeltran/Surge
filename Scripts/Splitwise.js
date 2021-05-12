f3285030d0bb7e33b6191f70a7aa97939b8ff312
Alonso Alejandro Zúñiga Beltrán <aazbeltran@outlook.com>
2021-05-11 23:51:27 -0500

    feat: enable add splitewise features

--- Scripts/Splitwise.js
+++ Scripts/Splitwise.js
@@ -4,4 +4,13 @@ if (obj.user !== undefined) {
     obj.user = {...obj.user, pro: true};
 }
 
+if (obj.metadata !== undefined && obj.metadata.features !== undefined) {
+    Object.entries(obj.metadata.features).map(([key, props]) => {
+        if (props.enabled !== undefined) {
+            props.enabled = true;
+        }
+        obj.metadata.features[key] = props;
+    })
+}
+
 $done({body: JSON.stringify(obj)});
